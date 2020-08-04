3896f28dd413aa0e6d9c874a80bc12534902daa6
Alexey D <ord.alwo@gmail.com>
2020-08-04 17:27:24 +0300

    Remove combineStates

--- test/lib/describe_state_test.js
+++ test/lib/describe_state_test.js
@@ -1,9 +1,10 @@
 import test from "enhanced-tape";
 import { createStore, combineReducers } from "redux";
-import describeState, { combineStates } from "describe_state";
+import describeState from "describe_state";
 
 test("describeState", function(t) {
   t.test("state.reducer", function(t) {
+
     t.test("sets initial state", function(t) {
       let { reducer } = describeState({name: "todos", getInitialState: () => []});
       let store = createStore(combineReducers({todos: reducer}));
@@ -57,32 +58,5 @@ test("describeState", function(t) {
   
       t.end();
     });
-  });
-});
-
-test("combineStates", function(t) {
-  t.test("combines states into reducers object", function(t) {
-    let state = describeState({name: "todos", getInitialState: () => [1]});
-    let store = createStore(combineReducers(combineStates([state])));
-
-    t.same(store.getState().todos, [1]);
-  
-    t.end();
-  });
-
-  t.test("throws instead of overwriting existing state", function(t) {
-    let todos_1 = describeState({name: "todos", getInitialState: () => []});
-    let todos_2 = describeState({name: "todos", getInitialState: () => []});
-
-    t.throws(() => combineStates([todos_1, todos_2]), /state/);
-  
-    t.end();
-  });
-
-  t.test("validates {name, reducer} to be present in state", function(t) {
-    t.throws(() => combineStates([{reducer: () => 1}]), /name/);
-    t.throws(() => combineStates([{name: "todos"}]), /reducer/);
-  
-    t.end();
   });
 });
